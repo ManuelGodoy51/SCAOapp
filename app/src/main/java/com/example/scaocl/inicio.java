d875206c385e1dd7696740198a18617a2b2837d2
DESKTOP-U9R6E57\Manuelillo <manuelalejandrogodoyvergara@gmail.com>
2021-12-07 01:16:26 -0300

    aplicación de botones agregar y listar productos, y creacion de vista agregar producto

--- app/src/main/java/com/example/scaocl/inicio.java
+++ app/src/main/java/com/example/scaocl/inicio.java
@@ -2,16 +2,28 @@ package com.example.scaocl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class inicio extends AppCompatActivity {
     TextView twUsuario;
+    Button btnAgregarProducto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
         recibirDatos();
+        btnAgregarProducto = (Button) findViewById(R.id.btnAgregarProducto);
+        btnAgregarProducto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(inicio.this, AgregarProductos.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void recibirDatos(){
