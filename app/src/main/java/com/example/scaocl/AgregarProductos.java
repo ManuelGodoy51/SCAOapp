30c961bf40012558992d2188b501ad52e1dc8c3e
DESKTOP-U9R6E57\Manuelillo <manuelalejandrogodoyvergara@gmail.com>
2021-12-13 19:27:25 -0300

    Agregar productos funcional

--- app/src/main/java/com/example/scaocl/AgregarProductos.java
+++ app/src/main/java/com/example/scaocl/AgregarProductos.java
@@ -1,10 +1,8 @@
 package com.example.scaocl;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -13,32 +11,26 @@ import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
-public class AgregarProductos extends AppCompatActivity {
+public class AgregarProductos extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener{
     TextView TVusuario;
     Spinner spTipoAlimento;
     EditText ETcodigoBarra,ETnombreP,ETDfechaRecepcion,ETnumeroGP,ETmarca,ETcantidad,
             ETDfechaVencimiento,ETlote,ETaccionCorrectiva,ETtemRecepcion,ETverificador,ETobservacion;
     Button BTNguardar;
     String Estado,Color,Textura,Olor,Aprovacion;
+
+    ProgressDialog progreso;
+    RequestQueue request;
+    JsonObjectRequest jsonObjectRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,28 +50,47 @@ public class AgregarProductos extends AppCompatActivity {
         ETverificador=findViewById(R.id.ETverificador);
         ETobservacion=findViewById(R.id.ETobservacion);
         BTNguardar=findViewById(R.id.BTNguardar);
+        request=Volley.newRequestQueue(this);
         recibirDatos();
         LlenadoSpinner();
 //datos para enviar codigo_barra,nombre_producto,numero_gp,marca,cantidad,lote,temrecepcion//
         BTNguardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                cargarWebService();
             }
         });
     }
+    private void cargarWebService(){
+        progreso = new ProgressDialog(this);
+        progreso.setMessage("Cargando...");
+        progreso.show();
+        String url = "http://192.168.1.7/ejemplologin/registroproducto.php?codigo_barra="+ETcodigoBarra.getText().toString()+
+                "&nombre_producto="+ETnombreP.getText().toString()+"&n_guia_despacho="+ETnumeroGP.getText().toString()+
+                "&marca="+ETmarca.getText().toString()+"&cantidad="+ETcantidad.getText().toString()+"&lote="+ETlote.getText().toString()+
+                "&t_recepcion="+ETtemRecepcion.getText().toString()+"";
+        url.replace(" ","%20");
 
+        jsonObjectRequest= new JsonObjectRequest(Request.Method.GET,url,null,this,this);
+        request.add(jsonObjectRequest);
+    }
 
+    @Override
+    public void onResponse(JSONObject response) {
+        Toast.makeText(getApplicationContext(),"Se ha registrado correctamente",Toast.LENGTH_SHORT).show();
+        progreso.hide();
+    }
 
-
-
-
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        progreso.hide();
+        Toast.makeText(getApplicationContext(),"No se pudo registrar "+error.toString(),Toast.LENGTH_SHORT).show();
+    }
 
     private void recibirDatos(){
         Bundle extra = getIntent().getExtras();
         String Nusuario = extra.getString("NombreUsuario");
         String IdUsuario = extra.getString("IdUsuario");
-
         TVusuario = (TextView) findViewById(R.id.TVusuario);
         TVusuario.setText(Nusuario);
     }
