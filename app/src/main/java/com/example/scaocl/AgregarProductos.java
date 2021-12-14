d502f056c80fa654ccc1b65f56d7ef2eae0fd3dc
DESKTOP-U9R6E57\Manuelillo <manuelalejandrogodoyvergara@gmail.com>
2021-12-14 00:10:45 -0300

    valor de spinner agregado a base de datos al igual que iduser y varios campos

--- app/src/main/java/com/example/scaocl/AgregarProductos.java
+++ app/src/main/java/com/example/scaocl/AgregarProductos.java
@@ -1,11 +1,17 @@
 package com.example.scaocl;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
@@ -18,10 +24,12 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class AgregarProductos extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener{
-    TextView TVusuario;
+    TextView TVusuario,TVidUsuario;
     Spinner spTipoAlimento;
     EditText ETcodigoBarra,ETnombreP,ETDfechaRecepcion,ETnumeroGP,ETmarca,ETcantidad,
             ETDfechaVencimiento,ETlote,ETaccionCorrectiva,ETtemRecepcion,ETverificador,ETobservacion;
@@ -31,7 +39,7 @@ public class AgregarProductos extends AppCompatActivity implements Response.List
     ProgressDialog progreso;
     RequestQueue request;
     JsonObjectRequest jsonObjectRequest;
-
+    private int dia,mes,ano,dia2,mes2,ano2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,23 +65,42 @@ public class AgregarProductos extends AppCompatActivity implements Response.List
         BTNguardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Bundle extra = getIntent().getExtras();
+                String IdUsuario = extra.getString("IdUsuario");
                 cargarWebService();
             }
         });
+        ETDfechaVencimiento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cargarfechaV();
+            }
+        });
+        ETDfechaRecepcion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cargarfechaR();
+            }
+        });
+
     }
-    private void cargarWebService(){
+    private void cargarWebService() {
+
         progreso = new ProgressDialog(this);
         progreso.setMessage("Cargando...");
         progreso.show();
-        String url = "http://192.168.1.7/ejemplologin/registroproducto.php?codigo_barra="+ETcodigoBarra.getText().toString()+
-                "&nombre_producto="+ETnombreP.getText().toString()+"&n_guia_despacho="+ETnumeroGP.getText().toString()+
-                "&marca="+ETmarca.getText().toString()+"&cantidad="+ETcantidad.getText().toString()+"&lote="+ETlote.getText().toString()+
-                "&t_recepcion="+ETtemRecepcion.getText().toString()+"";
+        String url = "http://192.168.1.7/ejemplologin/registroproducto.php?codigo_barra=" + ETcodigoBarra.getText().toString() +
+                "&nombre_producto=" + ETnombreP.getText().toString() + "&n_guia_despacho=" + ETnumeroGP.getText().toString() +
+                "&marca=" + ETmarca.getText().toString() + "&cantidad=" + ETcantidad.getText().toString() + "&lote=" + ETlote.getText().toString() +
+                "&t_recepcion=" + ETtemRecepcion.getText().toString() + "&fecha_recepcion="+ETDfechaRecepcion.getText().toString()+
+                "&fecha_vencimiento="+ETDfechaVencimiento.getText().toString()+"&id_user="+TVidUsuario.getText().toString()+"&id_tipo_alimento="+(spTipoAlimento.getSelectedItemId()+1)+
+                "&accion_correctiva="+ETaccionCorrectiva+"&verificador="+ETverificador+"&observacion="+ETobservacion+"";
         url.replace(" ","%20");
 
         jsonObjectRequest= new JsonObjectRequest(Request.Method.GET,url,null,this,this);
         request.add(jsonObjectRequest);
     }
+
 
     @Override
     public void onResponse(JSONObject response) {
@@ -92,8 +119,11 @@ public class AgregarProductos extends AppCompatActivity implements Response.List
         String Nusuario = extra.getString("NombreUsuario");
         String IdUsuario = extra.getString("IdUsuario");
         TVusuario = (TextView) findViewById(R.id.TVusuario);
+        TVidUsuario=(TextView)findViewById(R.id.TVidUser);
+        TVidUsuario.setText(IdUsuario);
         TVusuario.setText(Nusuario);
     }
+
     private void LlenadoSpinner(){
         ArrayList<TipoAlimento> tipoProducto = new ArrayList();
 
@@ -104,6 +134,7 @@ public class AgregarProductos extends AppCompatActivity implements Response.List
         ArrayAdapter<TipoAlimento> adapter = new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item,tipoProducto);
         spTipoAlimento.setAdapter(adapter);
     }
+
     public void onRadioButtonClicked(View view) {
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
@@ -160,5 +191,33 @@ public class AgregarProductos extends AppCompatActivity implements Response.List
                     break;
         }
 
+    }
+    ////////////Se carga la misma fecha en los dos edit text asi que se debe arreglar///////////////////
+    private void cargarfechaV(){
+        final Calendar c=Calendar.getInstance();
+        dia=c.get(Calendar.DAY_OF_MONTH);
+        mes=c.get(Calendar.MONTH);
+        ano=c.get(Calendar.YEAR);
+        DatePickerDialog datePickerDialog= new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                ETDfechaVencimiento.setText(ano+"-"+ (mes+1) +"-"+dia);
+            }
+        },ano,mes,dia);
+        datePickerDialog.show();
+    }
+
+    private void cargarfechaR(){
+        final Calendar c=Calendar.getInstance();
+        dia2=c.get(Calendar.DAY_OF_MONTH);
+        mes2=c.get(Calendar.MONTH);
+        ano2=c.get(Calendar.YEAR);
+        DatePickerDialog datePickerDialog= new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                ETDfechaRecepcion.setText(ano2+"-"+ (mes2+1) +"-"+dia2);
+            }
+        },ano2,mes2,dia2);
+        datePickerDialog.show();
     }
 }
