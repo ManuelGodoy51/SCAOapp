6e28bbdf77552fa3152d14c24563789734d50a16
DESKTOP-U9R6E57\Manuelillo <manuelalejandrogodoyvergara@gmail.com>
2021-12-28 04:45:58 -0300

    creación visual de listado productos

--- app/src/main/java/com/example/scaocl/ListarProducto.java
+++ app/src/main/java/com/example/scaocl/ListarProducto.java
@@ -3,11 +3,26 @@ package com.example.scaocl;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ListarProducto extends AppCompatActivity {
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.json.JSONObject;
+
+public class ListarProducto extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar_producto);
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+
     }
 }
